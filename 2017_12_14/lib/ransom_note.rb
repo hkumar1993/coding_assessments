d62d8b95ef59aad742efb4e31f1436be14f17b86
Harsh <harsh@hkumar.me>
2017-12-16 14:31:02 +0800

    Update tests for ransom note

--- 2017_12_14/lib/ransom_note.rb
+++ 2017_12_14/lib/ransom_note.rb
@@ -1,9 +1,20 @@
+require 'byebug'
+
 def ransom_note(magazine, ransom)
     return false if magazine.length < ransom.length
     ransom_chars = Hash.new(0)
+    magazine_chars = Hash.new(0)
+
+    magazine.chars.each do |c|
+        magazine_chars[c] += 1
+    end
+
     ransom.chars.each do |c|
         ransom_chars[c] += 1
     end
+
+    byebug
+
     magazine.chars.each do |c|
         ransom_chars[c] -= 1
     end
--- 2017_12_14/spec/ransom_note_spec.rb
+++ 2017_12_14/spec/ransom_note_spec.rb
@@ -12,4 +12,4 @@ end
 
 # 100 50
 # zahk dp apdz clo e dk awfvf osb qr sa cqjq zgr nvxtb abjy axa ili wdyw soqku buwcl qcub sautu ii vkrzl bdob nona al zg ombzc c dbun f xkuo lsax hfki j dfft uce ugj ywz vucgg xq udrkt ypy tmxgc ty gar kty dc bznj pzzx clo apdz nvxtb clo sa clo zahk awfvf soqku udrkt udrkt e ypy xkuo tmxgc ombzc wdyw al axa lsax clo abjy osb apdz bdob pzzx zahk c bznj gar osb xkuo zahk zg uce zg clo e apdz gar xq dbun buwcl ili bznj clo osb dc dbun ywz
-# buwcl qr axa ypy zahk nvxtb dp hfki ii uce dc zg dbun ypy ty cqjq zg kty bznj zg zahk dp c al ugj ywz qcub ywz wdyw hfki gar e axa dp qr kty bznj clo ty vucgg qcub al vkrzl qcub j awfvf soqku lsax bdob nvxtb
+# buwcl qr axa ypy zahk nvxtb dp hfki ii uce dc zg dbun ypy ty cqjq zg kty bznj zg zahk dp c z
