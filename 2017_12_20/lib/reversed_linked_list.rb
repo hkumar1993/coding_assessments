1cc41edbc163c2d031257463223e79f358d681e4
Harsh <harsh@hkumar.me>
2017-12-20 11:05:29 +0800

    Solve algorithm in O(n) time O(1) space

--- 2017_12_20/lib/reversed_linked_list.rb
+++ 2017_12_20/lib/reversed_linked_list.rb
@@ -1,4 +1,27 @@
 def reversed_linked_list(head)
     return head if head.next.nil?
     
+    prev_link = nil
+    current_link = head
+    next_link = current_link.next
+    
+    until next_link.nil?
+        current_link.next = prev_link
+        prev_link = current_link
+        current_link = next_link
+        next_link = current_link.next
+    end
+
+    current_link.next = prev_link
+    prev_link = current_link
+
+    prev_link
+
 end
+
+# nil -> A -> B -> C -> D -> nil
+# prev = nil
+# current = A
+# next = B
+# prev = A -> nil
+# 
