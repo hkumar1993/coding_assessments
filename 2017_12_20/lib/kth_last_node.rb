13c9225236dd939d3014948f940f27bfad354d2f
Harsh <harsh@hkumar.me>
2017-12-20 14:50:47 +0800

    Solve problem with n space and n time

--- 2017_12_20/lib/kth_last_node.rb
+++ 2017_12_20/lib/kth_last_node.rb
@@ -1,3 +1,9 @@
 def kth_last_node(k, head)
-
+    nodes = []
+    current = head
+    until current.nil?
+        nodes << current
+        current = current.next
+    end
+    nodes[nodes.length - k]
 end
