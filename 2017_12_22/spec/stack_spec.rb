b752885899798b20ef04cce8f90d3a7e179ababd
Harsh <harsh@hkumar.me>
2017-12-22 15:56:30 +0800

    Fix specs

--- 2017_12_22/spec/stack_spec.rb
+++ 2017_12_22/spec/stack_spec.rb
@@ -7,11 +7,11 @@ describe 'Stack' do
 
     describe '#initialize' do
         it 'initializes an empty stack' do
-            expect(empty_stack).to eq([])
+            expect(empty_stack.store).to eq([])
         end
         
         it 'initializes a stack with array value' do
-            expect(initialize_stack).to eq([1,2,3])
+            expect(initialize_stack.store).to eq([1,2,3])
         end
     end
 
@@ -22,8 +22,8 @@ describe 'Stack' do
     end
 
     describe '#pop' do
+        let(:popped){ initialize_stack.pop }
         it 'pops element from top of stack' do
-            let(:popped){ initialize_stack.pop }
             expect(popped).to eq(1)
             expect(initialize_stack.store).to eq([2,3])
         end
