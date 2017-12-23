79f66010652926de970595bc8f9ade1260487fb4
Harsh <harsh@hkumar.me>
2017-12-23 15:13:42 -0800

    Begin writing rqm solution

---
+++ 2017_12_23/lib/range_minimum_query.rb
@@ -0,0 +1,9 @@
+require_relative 'range_node'
+
+def range_minimum_query(arr, range_start, range_end)
+    
+end
+
+def make_segment_tree(arr, range_start = 0, range_start = arr.length)
+    return RangeNode.new(arr[range_start])
+end
