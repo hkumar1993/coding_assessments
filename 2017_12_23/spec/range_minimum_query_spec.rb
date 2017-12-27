904dafea0597a43327de07530334375ba49bc50a
Harsh <harsh@hkumar.me>
2017-12-26 19:24:06 -0800

    Update spec to test overlap

--- 2017_12_23/spec/range_minimum_query_spec.rb
+++ 2017_12_23/spec/range_minimum_query_spec.rb
@@ -1,5 +1,24 @@
 require 'range_minimum_query'
 
+describe "#range_overlap?" do
+
+    it "returns 0 if partial overlap" do
+        expect(range_overlap?((1..5),(2..4))).to eq(0)  
+        expect(range_overlap?((3..5),(2..4))).to eq(0)  
+        expect(range_overlap?((3..5),(2..4))).to eq(0)  
+    end
+    
+    it "returns 1 if total overlap" do
+        expect(range_overlap?((2..2),(2..4))).to eq(1)  
+        expect(range_overlap?((2..3),(2..4))).to eq(1)  
+        expect(range_overlap?((2..3),(1..5))).to eq(1)  
+    end
+    
+    
+
+end
+
+
 describe "#range_minimum_query" do
     
     let(:arr) { [ 31, 41, 59, 26, 53, 58, 97, 93 ] } 
