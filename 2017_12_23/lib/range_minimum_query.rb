802eb26280a8123bab1c126cc3d5629a9bcb64dd
Harsh <harsh@hkumar.me>
2017-12-27 11:18:10 -0800

    Complete RMQ

--- 2017_12_23/lib/range_minimum_query.rb
+++ 2017_12_23/lib/range_minimum_query.rb
@@ -2,11 +2,27 @@ require_relative 'range_node'
 
 def range_minimum_query(arr, range_start, range_end)
     root = make_segment_tree(arr)
-
+    find_minimum_node( root, range_start, range_end )
 end
 
 def find_minimum_node(node, range_start, range_end)
-    
+    overlap = range_overlap?((node.range_start..node.range_end),(range_start..range_end))
+    case overlap
+    when 1
+        return node.value
+    when 0
+        left = find_minimum_node( node.left, range_start, range_end )
+        right = find_minimum_node( node.right, range_start, range_end )
+        return minimum_value( left, right )
+    when -1
+        return nil
+    end
+end
+
+def minimum_value(a,b)
+    return a if b.nil?
+    return b if a.nil?
+    return a < b ? a : b
 end
 
 def range_overlap?(node_range, query_range)
