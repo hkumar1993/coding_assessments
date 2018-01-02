e2446db44f3c3fcffab136d55713310774784e0d
Harsh <harsh@hkumar.me>
2018-01-02 12:50:09 -0800

    Write spec to test memoization version

--- 2018_01_01/spec/coin_base_spec.rb
+++ 2018_01_01/spec/coin_base_spec.rb
@@ -1,8 +1,14 @@
 require 'coin_base'
 
-describe "#coin_base" do
+describe "#coin_base_recursive" do
     it "returns an array of possible combinations of coins to make the amount of money" do
-        expect(coin_base(4,[1,2,3])).to include([1,1,1,1], [1,1,2], [1,3], [2,2])
+        expect(coin_base_recursive(4,[1,2,3])).to include([1,1,1,1], [1,1,2], [1,3], [2,2])
     end
-    
 end
+
+describe "#coin_base_memo" do
+     it "returns an array of possible combinations of coins to make the amount of money" do
+        expect(coin_base_memo(4,[1,2,3])).to include([1,1,1,1], [1,1,2], [1,3], [2,2])
+    end
+end
+
