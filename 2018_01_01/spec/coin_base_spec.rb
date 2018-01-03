cb183e837b429c1e87557366bdd20ae91dd6b5df
Harsh <harsh@hkumar.me>
2018-01-02 16:07:23 -0800

    Add new version of coin base test

--- 2018_01_01/spec/coin_base_spec.rb
+++ 2018_01_01/spec/coin_base_spec.rb
@@ -2,13 +2,20 @@ require 'coin_base'
 
 describe "#coin_base_recursive" do
     it "returns an array of possible combinations of coins to make the amount of money" do
-        expect(coin_base_recursive(4,[1,2,3])).to include([1,1,1,1], [1,1,2], [1,3], [2,2])
+        expect(coin_base_recursive(4,[1,2,3])).to include([1,1,1,1], [2,1,1], [3,1], [2,2])
     end
 end
 
 describe "#coin_base_memo" do
-     it "returns an array of possible combinations of coins to make the amount of money" do
-        expect(coin_base_memo(4,[1,2,3])).to include([1,1,1,1], [1,1,2], [1,3], [2,2])
+    it "returns an array of possible combinations of coins to make the amount of money" do
+        CoinClass = Coin.new
+        expect(CoinClass.coin_base_memo(4,[1,2,3])).to include([1,1,1,1], [2,1,1], [3,1], [2,2])
+    end
+end
+
+describe "#coin_base" do
+    it "returns an array of possible combinations of coins to make the amount of money" do
+        expect(coin_base(4,[1,2,3])).to include([1,1,1,1], [2,1,1], [3,1], [2,2])
     end
 end
 
