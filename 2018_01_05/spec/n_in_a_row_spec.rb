7e5523297307f5d2da2a9aeacc48a6f6fd554a90
Harsh <harsh@hkumar.me>
2018-01-05 20:18:06 -0800

    Update specs

--- 2018_01_05/spec/n_in_a_row_spec.rb
+++ 2018_01_05/spec/n_in_a_row_spec.rb
@@ -1,16 +1,32 @@
 require 'n_in_a_row'
 
 describe "#check_win?" do
-    let(:win_input) { 
+    let(:input) { 
         [
             ['x','','o'],
             ['x','o',''],
             ['x','', '']
         ]
     }
+    let(:single_input) { 
+        [
+            ['x','','o'],
+            ['o','o','o'],
+            ['x','', '']
+        ]
+    }
+
     it "checks if the current element is n-in-a-row" do
         expect(check_win?(3, 'x', 0, 0, input)).to be_truthy
-        expect(check_win?(3, '0', 0, 2, input)).to be_truthy
+        expect(check_win?(1, 'x', 0, 0, input)).to be_truthy
+        expect(check_win?(2, 'o', 1, 0, single_input)).to be_truthy
     end
+
+    it "returns false if current element is not n-in-a-row" do
+        expect(check_win?(4, 'x', 0, 0, input)).to be_falsey
+        expect(check_win?(3, 'o', 0, 2, input)).to be_falsey
+        expect(check_win?(2, 'x', 0, 0, single_input)).to be_falsey
+    end
+    
     
 end
