1e38dc7a2cbe8f9e70ab25ac78da49d772bc3aa2
Harsh <harsh@hkumar.me>
2018-01-05 19:22:02 -0800

    Begin n in a row

---
+++ 2018_01_05/lib/n_in_a_row.rb
@@ -0,0 +1,25 @@
+# Write a method that would find
+# if a certain character comes n-times in a row
+# either vertically, horizonatally, or diagonally
+
+# INPUT
+# n = total number of characters required to be in a row
+# input = 2d array
+
+# [
+#     ['x','','o'],
+#     ['x','o',''],
+#     ['x','', '']
+# ]
+
+def n_in_a_row(n, input)
+    winner = nil
+    input.each_with_index do |row, row_id|
+        row.each_with_index do |el, col_id|
+            winner = el if check_win?(n, el, row_id, col_id)
+            return winner unless winner.nil?
+        end
+    end
+    winner
+end
+
