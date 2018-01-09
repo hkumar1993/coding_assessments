3e481119a9b3f8f3e872e964628f0d97648cfd18
Harsh <harsh@hkumar.me>
2018-01-09 15:11:40 -0800

    Write test for second largest element

---
+++ 2018_01_09/spec/second_largest_element_spec.rb
@@ -0,0 +1,17 @@
+require 'binary_tree_node'
+require 'second_largest_element'
+
+describe "#second_largest_element" do
+    let(:tree_sample) { build_tree([1,2,3,4,5,6,7,8,9].shuffle) } 
+    context "Helper method to #find_max" do
+        it "finds maximum value of tree" do
+            expect(find_max(tree_sample)).to  eq(9)
+        end
+    end
+
+    it "finds the second largest element" do
+        expect(second_largest_element(tree_sample)).to  eq(8)
+    end
+    
+    
+end
