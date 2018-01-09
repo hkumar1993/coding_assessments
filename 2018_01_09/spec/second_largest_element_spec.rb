65d982060c554ee4e81190f9e016cefe350ca41f
Harsh <harsh@hkumar.me>
2018-01-09 15:45:22 -0800

    Update spec

--- 2018_01_09/spec/second_largest_element_spec.rb
+++ 2018_01_09/spec/second_largest_element_spec.rb
@@ -5,12 +5,12 @@ describe "#second_largest_element" do
     let(:tree_sample) { build_tree([1,2,3,4,5,6,7,8,9].shuffle) } 
     context "Helper method to #find_max" do
         it "finds maximum value of tree" do
-            expect(find_max(tree_sample)).to  eq(9)
+            expect(find_max(tree_sample).value).to  eq(9)
         end
     end
 
     it "finds the second largest element" do
-        expect(second_largest_element(tree_sample)).to  eq(8)
+        expect(second_largest_element(tree_sample).value).to  eq(8)
     end
     
     
