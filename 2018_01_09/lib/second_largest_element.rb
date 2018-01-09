f5ecfce3505e0cfd7714fbc73ea1a24e00c17f8a
Harsh <harsh@hkumar.me>
2018-01-09 15:45:33 -0800

    Optimize method

--- 2018_01_09/lib/second_largest_element.rb
+++ 2018_01_09/lib/second_largest_element.rb
@@ -1,15 +1,41 @@
 def second_largest_element(root)
-    parent_node = root
-    child_node = root.right
-    raise ArgumentError if child_node.nil?
-    until child_node.right.nil?
-        parent_node = child_node
-        child_node = child_node.right
+    raise ArgumentError if root.right.nil? && root.left.nil?
+
+    current_node = root
+    second = nil
+    previous_node = current_node
+    while current_node
+        display_node("Current", current_node)
+        display_node("Previous", previous_node)
+        display_node("Second", second)
+        display_node("Left", current_node.left)
+        display_node("Right", current_node.right)
+        puts "~~~~~~~~~~~"
+        if current_node.left && !current_node.right
+            second = find_max(current_node.left)
+        else
+            if current_node.right && !current_node.right.right
+                second = current_node
+            end
+        end
+        previous_node = current_node
+        current_node = current_node.right
     end
-    parent_node.value
+    display_node("Current", current_node)
+    display_node("Previous", previous_node)
+    display_node("Second", second)
+    # display_node("Left", current_node.left)
+    # display_node("Right", current_node.right)
+    puts "~~~~~~~~~~~"
+    !!second ? second : previous_node
+    
 end
 
 def find_max(root)
-    return root.value if root.right.nil?
+    return root if root.right.nil?
     find_max(root.right)
+end
+
+def display_node(label, node)
+    puts "#{label}: #{!!node ? node.value : "NIL"}"
 end
