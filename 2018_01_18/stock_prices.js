d06ad13bf9f59e250b4233665ca22609e7b399dd
Harsh <harsh@hkumar.me>
2018-01-18 08:58:37 -0800

    Finish personal solution

--- 2018_01_18/stock_prices.js
+++ 2018_01_18/stock_prices.js
@@ -1,4 +1,4 @@
-let stockPricesYesterday = [10, 7, 5, 8, 11, 9]
+let stockPricesYesterday = [10, 9, 8, 7, 6, 5]
 
 function getMaxProfit(stockPrices){
     let max = {
@@ -17,7 +17,7 @@ function getMaxProfit(stockPrices){
 
     stockPrices.forEach(function(price, index){
 
-        const current_gain = tempMax.value - tempMin.value
+        const current_gain = tempMax.value - tempMin.valuea
 
         if (price < tempMin.value) {
             tempMin.value = price
@@ -28,5 +28,36 @@ function getMaxProfit(stockPrices){
             tempMax.value = price
             tempMax.index = index
         }
+
+        finalMinMax = checkMax( tempMin, tempMax, min, max )
+        max = finalMinMax.max
+        min = finalMinMax.min
     })
+    return diff(max,min)
 }
+
+function checkMax( tempMin, tempMax, min, max ) {
+    const currentDiff = diff(max, min)
+    const tempDiff = diff(tempMax, tempMin)
+    if (tempDiff > currentDiff){
+        return {
+            max: tempMax,
+            min: tempMin
+        }
+    } else {
+        return {
+            max,
+            min
+        }
+    }
+}
+
+function diff (max, min){
+    if(max.index > min.index){
+        return max.value - min.value
+    } else {
+        return -Infinity
+    }
+}
+
+console.log(getMaxProfit(stockPricesYesterday))
