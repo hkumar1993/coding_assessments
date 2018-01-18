99a38f3a124280cfea3223ce4f368ed5b5c61283
Harsh <harsh@hkumar.me>
2018-01-18 09:14:57 -0800

    Optimize function

--- 2018_01_18/stock_prices.js
+++ 2018_01_18/stock_prices.js
@@ -1,63 +1,80 @@
-let stockPricesYesterday = [10, 9, 8, 7, 6, 5]
+let stockPricesYesterday = [10, 7, 5, 8, 11, 9]
 
 function getMaxProfit(stockPrices){
-    let max = {
-        value: -Infinity,
-        index: null
+    let minPrice = stockPrices[0]
+    let currentMax = 0
+
+    for (let i = 1; i < stockPrices.length; i++) {
+        const currentPrice = stockPrices[i];
+        const potentialProfit = currentPrice - minPrice
+
+        currentMax = Math.max(currentMax, potentialProfit)
+
+        minPrice = Math.min(minPrice, currentPrice)
     }
 
-    let min = {
-        value: Infinity,
-        index: null
-    }
+    return currentMax
 
-    let tempMax = Object.assign({}, max)
+}
 
-    let tempMin = Object.assign({}, min)
+// function getMaxProfit(stockPrices){
+//     let max = {
+//         value: -Infinity,
+//         index: null
+//     }
 
-    stockPrices.forEach(function(price, index){
+//     let min = {
+//         value: Infinity,
+//         index: null
+//     }
 
-        const current_gain = tempMax.value - tempMin.valuea
+//     let tempMax = Object.assign({}, max)
 
-        if (price < tempMin.value) {
-            tempMin.value = price
-            tempMin.index = index
-        }
+//     let tempMin = Object.assign({}, min)
+
+//     stockPrices.forEach(function(price, index){
+
+//         const current_gain = tempMax.value - tempMin.valuea
+
+//         if (price < tempMin.value) {
+//             tempMin.value = price
+//             tempMin.index = index
+//         }
         
-        if (price > tempMax.value){
-            tempMax.value = price
-            tempMax.index = index
-        }
+//         if (price > tempMax.value){
+//             tempMax.value = price
+//             tempMax.index = index
+//         }
 
-        finalMinMax = checkMax( tempMin, tempMax, min, max )
-        max = finalMinMax.max
-        min = finalMinMax.min
-    })
-    return diff(max,min)
-}
+//         finalMinMax = checkMax( tempMin, tempMax, min, max )
+//         max = finalMinMax.max
+//         min = finalMinMax.min
+//     })
+//     return diff(max,min)
+// }
 
-function checkMax( tempMin, tempMax, min, max ) {
-    const currentDiff = diff(max, min)
-    const tempDiff = diff(tempMax, tempMin)
-    if (tempDiff > currentDiff){
-        return {
-            max: tempMax,
-            min: tempMin
-        }
-    } else {
-        return {
-            max,
-            min
-        }
-    }
-}
+// function checkMax( tempMin, tempMax, min, max ) {
+//     const currentDiff = diff(max, min)
+//     const tempDiff = diff(tempMax, tempMin)
+//     if (tempDiff > currentDiff){
+//         return {
+//             max: tempMax,
+//             min: tempMin
+//         }
+//     } else {
+//         return {
+//             max,
+//             min
+//         }
+//     }
+// }
 
-function diff (max, min){
-    if(max.index > min.index){
-        return max.value - min.value
-    } else {
-        return -Infinity
-    }
-}
+// function diff (max, min){
+//     if(max.index > min.index){
+//         return max.value - min.value
+//     } else {
+//         return -Infinity
+//     }
+// }
 
 console.log(getMaxProfit(stockPricesYesterday))
