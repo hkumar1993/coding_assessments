68a7c873d1965086e6e8cd9b453fcf8a7b9848c1
Harsh <harsh@hkumar.me>
2018-01-23 20:17:56 -0800

    update find duplicate

--- 2018_01_22/find_duplicate.js
+++ 2018_01_22/find_duplicate.js
@@ -1,17 +1,19 @@
 function findDuplicate(array){
     for (let i = 0; i < array.length; i++) {
-        console.log(array)
-        let pointer = Math.abs(array[i]-1)
+        let pointer = Math.abs(array[i]) - 1
         let numberAtPointer = array[pointer]
+        console.log(array)
+        console.log(pointer)
+        console.log(numberAtPointer)
         if(numberAtPointer > 0){
             array[pointer] = -numberAtPointer
         } else {
-            return Math.abs(numberAtPointer)
+            return Math.abs(array[i])
         }
     }
     return -1
 }
 
-const testArray = [1,2,2]
+const testArray = [8, 4, 6, 2, 6, 4, 7, 9, 5, 8]
 
 console.log(findDuplicate(testArray))
